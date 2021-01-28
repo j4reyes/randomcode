da357d56fba80172e6b5729272f71c013c836ce3
j4reyes <76570190+j4reyes@users.noreply.github.com>
2021-01-28 14:00:02 -0500

    Update BalloonHeight.java

--- BalloonHeight.java
+++ BalloonHeight.java
@@ -1,3 +1,9 @@
+/*
+Code used to determine if a balloon touches the ground within a certain timeframe, with a certain level of humidity.
+Creator: Jacob Reyes
+Source problem: https://dmoj.ca/problem/ccc11j2
+*/
+
 import java.lang.Math; 
 import java.util.Scanner;
 class BalloonHeight{
@@ -6,6 +12,7 @@ Scanner input = new Scanner(System.in);
 int humidity = input.nextInt();
 int maxHours = input.nextInt();
 for(int t=1;t<=maxHours;t++){
+        //Checks if −6t^4 + ht^3 + 2t^2 + t is equal to 0, with inputted h and incrementally increasing t values.
         if((Math.pow(t, 4)*(-6) + (Math.pow(t, 3))*humidity + (Math.pow(t, 2))*2 + t)<=0){
             System.out.printf("%s%n%d","The balloon first touches ground at hour:",t);
             break;
